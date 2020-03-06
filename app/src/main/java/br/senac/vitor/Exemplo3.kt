6e5790d88ead56a92b3a1d9f2484b406f0d0bb77
vitor.ghost@hotmail.com <Mousehop10>
2020-03-05 22:24:32 -0300

    SEGUNDO COMMMIT

--- app/src/main/java/br/senac/vitor/Exemplo3.kt
+++ app/src/main/java/br/senac/vitor/Exemplo3.kt
@@ -3,14 +3,13 @@ package br.senac.vitor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_exemplo3.*
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.etNome
 
 class Exemplo3 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exemplo3)
+
 
         btnMostrar.setOnClickListener{
             var msg = """Nome: ${etNome.text}
---
+++ app/src/main/java/br/senac/vitor/atividade1.kt
@@ -0,0 +1,12 @@
+package br.senac.vitor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class atividade1 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_atividade1)
+    }
+}
